81d2fee0ba0f90701a8fa7ae3ef93a8e318e029d
silva <thediamondheart890@gmail.com>
2023-01-24 20:39:01 +0400

    add imgs of selections and winning and losing sounds

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ const rock = document.querySelector('.Rock');
 const paper = document.querySelector('.Paper');
 const scissors = document.querySelector('.Scissors');
 const bod = document.querySelector('body');
-const audio = document.querySelector('audio');
-const gameContent = document.querySelector('.game-content')
-const divOfScores = document.querySelector('.Score');
+const clickAudio = document.querySelector('.click-sound');
+const winAudio = document.querySelector('.win');
+const loseAudio = document.querySelector('.lose');
+const gameContent = document.querySelector('.game-content');
+const divOfScores = document.querySelector('.score');
+const selectionWithScoresContainer = document.querySelector('.score-selection')
+
+const imgOfPlayerSelection = document.createElement('img');
+const imgOfComputerSelection = document.createElement('img');
+
+imgOfPlayerSelection.classList.add('selection-img');
+imgOfComputerSelection.classList.add('selection-img');
 
 const divOfSelection = document.createElement('div');
 divOfSelection.classList.add('Selection');
@@ -58,16 +67,46 @@ let computerScore = 0;
 
 
 function playRound(e) {
-    audio.currentTime = 0;
-    audio.play();
+    clickAudio.currentTime = 0;
+    clickAudio.play();
     divOfScores.textContent = `You:${playerScore} - Computer:${computerScore}`;
     gameContent.insertBefore(divOfSelection,options );
 
-    let classValue = this.classList.value;
     let computerSelection = getComputerChoice();
+    let classValue = this.classList.value;
     let splittedClassValue = classValue.split(" ");
     let playerSelection = splittedClassValue[0];
-    
+
+    switch (playerSelection) {
+        case "Rock":
+        imgOfPlayerSelection.setAttribute('src', '../imgs/Rock.png')
+        selectionWithScoresContainer.insertBefore(imgOfPlayerSelection,divOfScores);
+        break;
+        case "Paper":
+        imgOfPlayerSelection.setAttribute('src', '../imgs/Paper.png')
+        selectionWithScoresContainer.insertBefore(imgOfPlayerSelection,divOfScores);
+        break;
+        case "Scissors":
+        imgOfPlayerSelection.setAttribute('src', '../imgs/Scissors.png')
+        selectionWithScoresContainer.insertBefore(imgOfPlayerSelection,divOfScores);
+        break;
+    }
+
+    switch (computerSelection) {
+        case "Rock":
+        imgOfComputerSelection.setAttribute('src', '../imgs/Rock.png')
+        selectionWithScoresContainer.appendChild(imgOfComputerSelection);
+        break;
+        case "Paper":
+        imgOfComputerSelection.setAttribute('src', '../imgs/Paper.png')
+        selectionWithScoresContainer.appendChild(imgOfComputerSelection);
+        break;
+        case "Scissors":
+        imgOfComputerSelection.setAttribute('src', '../imgs/Scissors.png')
+        selectionWithScoresContainer.appendChild(imgOfComputerSelection);
+        break;
+    }
+   
     divOfSelection.textContent = `Your Selection was: ${playerSelection}  |  Computer selection was: ${computerSelection}\n`
     divOfSelection.appendChild(divOfRoundResult);
 
@@ -107,15 +146,20 @@ function playRound(e) {
     if (playerScore === 5 && computerScore < 5){
         divOfRoundResult.textContent= "Congrats! You smashed the computer";
         divOfScores.textContent = `You:${playerScore} - Computer:${computerScore}`;
+        winAudio.currentTime = 0;
+        winAudio.play();
         playerScore = 0;
         computerScore = 0;
     }
     else if (playerScore < 5 && computerScore === 5){
         divOfRoundResult.textContent = "Unfortunately, The computer smashed you :(";
         divOfScores.textContent = `You:${playerScore} - Computer:${computerScore}`;
+        loseAudio.currentTime = 0;
+        loseAudio.play();
         playerScore = 0;
         computerScore = 0;
     }
+
     divOfScores.textContent = `You:${playerScore} - Computer:${computerScore}`;
-     
+       
 }
